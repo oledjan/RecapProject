e5dadc1375cbc0a323f57146513f4b8806666617
Oleg_Dziabenko <dziabenkoo@gmail.com>
2021-06-30 18:32:15 -0500

    Working on recap  day 1

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("aHello World");
-        System.out.println("Recap");
+        System.out.println("Recaping like day 1");
     }
 }
