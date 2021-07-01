2e5f991ab5ef72c7f8c164ce3e61dabaeb1291b1
Oleg_Dziabenko <dziabenkoo@gmail.com>
2021-06-30 20:19:57 -0500

    User Story 102 has finished

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -1,7 +1,8 @@
 public class UserStory102 {
 
 
-    // lots of code has been written here
+    //lots of code has been written here
     //ready to push this branch to remote
-    // and ope
+    //and open pull request so I can start collaborating
+    //so my team member can comment review my code and approve or request for more changes
 }
